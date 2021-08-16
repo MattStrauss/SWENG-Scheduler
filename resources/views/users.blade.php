a1d3791ad81588f0485e94ce7459ba3d13658c2f
570studio <strausstester@gmail.com>
2021-08-16 07:53:17 -0400

    Improve Mobile Disclaimer View

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -6,14 +6,15 @@
         <ul class="list-disc px-12">
 
             @foreach($users as $user)
-                @if ($loop->first)
 
+                @if ($loop->first)
                     <li class="mb-6">
                 @else
-                <li class="my-6">
+                    <li class="my-6">
                 @endif
+
                     {{$user->name}} - {{$user->email}} <br>
-                    <span class="my-2 text-sm text-gray-500">Account Created:{{$user->created_at->diffForHumans()}} </span>
+                    <span class="my-2 text-sm text-gray-400">Account Created: {{$user->created_at->diffForHumans()}} </span>
                 </li>
 
             @endforeach
